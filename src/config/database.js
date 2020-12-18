7767cb131cca43a81586a3fc7c0ed7d15bd31a9b
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2020-12-18 14:09:18 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ module.exports = {
   dialect: process.env.DB_DIALECT || 'postgres',
   type: 'postgres',
   url: 'postgres://postgres:200288@db:5432/green-api',
-  host: 'db',
+  host: 'ec2-52-201-184-16.compute-1.amazonaws.com',
   username: 'postgres',
   password: 200288,
   database: 'green-api',
