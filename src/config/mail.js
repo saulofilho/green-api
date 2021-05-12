81b59248f356c4fd51940046f9db49c12ead4a0d
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2021-05-12 13:55:46 -0300

    [fix] email prod

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,31 +1,44 @@
-if (process.env.NODE_ENV === 'development') {
-  console.log('DEV');
-  module.exports = {
-    service: process.env.MAIL_SERVICE,
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: false,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
-    default: {
-      from: 'Botanic Daily Data <hi@buenavistalab.com>',
-    },
-  };
-} else {
-  console.log('PROD');
-  module.exports = {
-    service: process.env.MAIL_SERVICE_PROD,
-    host: process.env.MAIL_HOST_PROD,
-    port: process.env.MAIL_PORT_PROD,
-    secure: false,
-    auth: {
-      user: process.env.MAIL_USER_PROD,
-      pass: process.env.MAIL_PASS_PROD,
-    },
-    default: {
-      from: 'Botanic Daily Data <hi@buenavistalab.com>',
-    },
-  };
-}
+// if (process.env.NODE_ENV === 'development') {
+//   console.log('DEV');
+//   module.exports = {
+//     service: process.env.MAIL_SERVICE,
+//     host: process.env.MAIL_HOST,
+//     port: process.env.MAIL_PORT,
+//     secure: false,
+//     auth: {
+//       user: process.env.MAIL_USER,
+//       pass: process.env.MAIL_PASS,
+//     },
+//     default: {
+//       from: 'Botanic Daily Data <hi@buenavistalab.com>',
+//     },
+//   };
+// } else {
+//   console.log('PROD');
+//   module.exports = {
+//     service: process.env.MAIL_SERVICE_PROD,
+//     host: process.env.MAIL_HOST_PROD,
+//     port: process.env.MAIL_PORT_PROD,
+//     secure: false,
+//     auth: {
+//       user: process.env.MAIL_USER_PROD,
+//       pass: process.env.MAIL_PASS_PROD,
+//     },
+//     default: {
+//       from: 'Botanic Daily Data <hi@buenavistalab.com>',
+//     },
+//   };
+// }
+module.exports = {
+  service: process.env.MAIL_SERVICE_PROD,
+  host: process.env.MAIL_HOST_PROD,
+  port: process.env.MAIL_PORT_PROD,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER_PROD,
+    pass: process.env.MAIL_PASS_PROD,
+  },
+  default: {
+    from: 'Botanic Daily Data <hi@buenavistalab.com>',
+  },
+};
